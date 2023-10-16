45b26884b8f63eb09711fd23bcfd32bda5ff9dba
Dano <danocolombo@gmail.com>
2023-10-15 21:43:49 -0400

    added logo to auth/index

--- app/(app)/auth/index.tsx
+++ app/(app)/auth/index.tsx
@@ -1,19 +1,44 @@
 import Screen from '@/common/components/Screen';
+import {
+    Text as RNText,
+    View as RNView,
+    Image,
+    StyleSheet,
+} from 'react-native';
 import Text from '@/common/components/Text';
+import View from '@/common/components/View';
+import Logo from '@/assets/images/logo.png';
 import LoginButton from '@/features/auth/LoginButton';
 
-// Define the AuthScreen component, intended as a starting point for authentication
-export default function () {
-	return (
-		<Screen
-			style={{
-				alignItems: 'center',
-				justifyContent: 'center',
-			}}>
-			<Text fontSize={24} fontFamily='SFHeavy'>
-				Welcome
-			</Text>
-			<LoginButton />
-		</Screen>
-	);
+export default function AuthScreen() {
+    return (
+        <Screen
+            style={{
+                flex: 1, // Take up the entire screen
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
+        >
+            <RNView style={styles.logoContainer}>
+                <Image source={Logo} style={styles.logo} resizeMode='contain' />
+            </RNView>
+            <Text fontSize={24} fontFamily='SFHeavy'>
+                Moolah
+            </Text>
+            <LoginButton />
+        </Screen>
+    );
 }
+
+const styles = StyleSheet.create({
+    logoContainer: {
+        width: '100%',
+        height: 200, // Set an appropriate height
+        alignItems: 'center', // Center the child elements vertically
+    },
+    logo: {
+        width: '95%',
+        height: undefined,
+        aspectRatio: 16 / 9,
+    },
+});
